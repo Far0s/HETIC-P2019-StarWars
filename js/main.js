3376222ac0a581ee78f5ce025e1a0700dc7439a2
Sebastien Dancer-Michel <s.dancermichel@gmail.com>
2015-12-17 13:15:18 +0100

    randomizing functions, init

--- js/main.js
+++ js/main.js
@@ -1,19 +1,72 @@
+// GLOBAL VARS
+var formOption = document.querySelectorAll('.form-selector-option'),
+formOptionSelected = document.querySelector('.form-selector-option.selected'),
+colorOption = document.querySelectorAll('.color-selector-option'),
+colorOptionSelected = document.querySelector('.color-selector-option').querySelector('[selected]'),
+randomBtn = document.querySelector('.randomall-btn'),
+    formChosen = 'spherical', // default
+    colorChosen = 'blue', // default
+    bgChosen = "1"; // default
+
+
+// General Init function, updates the droid
+function init() {
+  // Get all relevant vars : form, color, background, bonus
+  // Draw droid from thos vars
+  console.log('INIT');
+}
+
+
 // Droid Form Selector
+function formSelector() {
+  formChosen = this.getAttribute('value');
+  console.log(formChosen);
+  init();
+}
+
+for (var i = 0; i < formOption.length; i++) {
+  formOption[i].addEventListener('click', formSelector);
+}
 
 
 // Droid Color Scheme Selector
+function colorSelector() {
+  colorChosen = this.getAttribute('value');
+  console.log(colorChosen);
+  init();
+}
+
+for (var i = 0; i < colorOption.length; i++) {
+  colorOption[i].addEventListener('click', colorSelector);
+}
 
 
 // Droid Total Randomizer (random form + color + name)
+function randomAll() {
+  var randForm = ['spherical', 'circular'],
+  randColor = ['blue', 'green', 'red'];
+  formChosen = randForm[Math.floor(Math.random() * randForm.length)];
+  colorChosen = randColor[Math.floor(Math.random() * randColor.length)];
+  randomName();
+  console.log(formChosen);
+  console.log(colorChosen);
+  // Add random background
+  // Add random bonus
+  init();
+}
 
+randomBtn.addEventListener('click', randomAll);
 
 
 
 // Name Randomizer
-document.querySelector('.sbx-preview-name button').addEventListener('click', function(){
+function randomName() {
   var droidNames = ['R2-D2', 'R4-T5', 'R7-F5', 'R3-S6', 'R6-H5', 'R5-P8', 'R7-D4', 'R2-KT', 'R8-B7', 'R5-O13', 'R4-C4', 'R5-S9', 'R7-A4', 'R2-C2', 'R4-P17'];
   document.querySelector('.sbx-preview-name input').value = droidNames[parseInt(Math.random() * (droidNames.length))];
-});
+  init();
+}
+
+document.querySelector('.sbx-preview-name button').addEventListener('click', randomName);
 
 
 
@@ -37,7 +90,7 @@ var inventoryItem = document.querySelectorAll('.sbx-inventory-item');
 for (var i = 0; i < inventoryItem.length; i++) {
   inventoryItem[i].addEventListener('click', function() {
     var dataCat = this.getAttribute('data-cat'),
-        dataItem = this.getAttribute('data-item');
+    dataItem = this.getAttribute('data-item');
     if (dataCat == 'accessories'){
       console.log(dataItem);
       if (!viewbox.querySelector('[data-item='+dataItem+']')) {
